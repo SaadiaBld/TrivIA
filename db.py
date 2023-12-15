5dbe812bcfdd0466269d08c26d089793f71a288c
Quentin <jakimpact@gmail.com>
2023-12-15 14:47:32 +0100

    Update csv et db.py

--- db.py
+++ db.py
@@ -1,19 +1,29 @@
 import sqlite3
 import csv
 
-def create_database():
+def get_database(func):
 
-    db = sqlite3.connect("trivIA")
+    def wrap():
+        con = sqlite3.connect("trivIA.db")
+        cur = con.cursor()
+        func(cur)
+        con.commit()
+        con.close()
+        return wrap
 
-""""
-CREATE TABLE Questions (
-id primary key,
-theme varchar(255),
-difficulty varchar(255),
-question text,
-correct_answer varchar(255),
-incorrect_answer_1 varchar(255),
-incorrect_answer_2 varchar(255),
-incorrect_answer_3 varchar(255),
-);
-"""
+
+@get_database
+def create_table(cur):
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS Questions (id int PRIMARY KEY, category varchar(255), difficulty varchar(255), question text, correct_answer varchar(255), incorrect_answer_1 varchar(255), incorrect_answer_2 varchar(255), incorrect_answer_3 varchar(255))"
+    )
+
+@get_database
+def update_table(cur):
+    with open("questions.csv", "r") as file:
+        reader = csv.reader(file)
+    
+
+@get_database
+def delete_table(cur):
+    cur
