919d5f51639bd58ce81ed7d96948e9ef1e983256
cjulliar <cyrjulliard@gmail.com>
2023-12-21 09:25:15 +0100

    avance de l'IHM

--- src/ihm.py
+++ src/ihm.py
@@ -7,7 +7,10 @@ class Ihm:
         self.largeur = largeur
         self.hauteur = hauteur
         self.fenetre = pygame.display.set_mode((self.largeur, self.hauteur))
+        self.pos = {}
         pygame.display.set_caption("Fenêtre Pygame")
+        self.pos_player = 26
+        self.size_cercle = 19
 
     def color_choice(self, i):
         blue = (0, 0, 255)
@@ -31,11 +34,10 @@ class Ihm:
             color = green
         return color
 
-    def afficher(self):
+    def afficher_board(self):
         running = True
 
 
-        size_cercle = 19
 
         while running:
             self.fenetre.fill((0, 0, 0))  # Remplit la fenêtre avec du noir
@@ -47,6 +49,8 @@ class Ihm:
             centre_x -= 30
             centre_y -= 30
 
+            
+
             for i in range(42):
                 angle = i * (360 / 42)  # Angle pour chaque rond
                 x = centre_x + int(rayon_cercle * math.cos(math.radians(angle))) # Calcul de la position x
@@ -55,38 +59,125 @@ class Ihm:
                 # Dessin du rond
                 color = self.color_choice(i)
 
-                pygame.draw.circle(self.fenetre, color, (x, y), size_cercle)
+                pygame.draw.circle(self.fenetre, color, (x, y), self.size_cercle)
+
+                self.pos[i] = (x, y)
 
                 if angle == 0:
                     for c in range(6):
-                        pygame.draw.circle(self.fenetre, self.color_choice(c), (x - 46 * c - 46, y), size_cercle)
+                        xp = x - 46 * c - 46
+                        self.pos[100+c] = (xp, y)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c+4), (xp, y), self.size_cercle)
+                elif angle == 60:
+                    for c in range(6):
+                        xp = x - 23 * c - 23
+                        yp = y - 40 * c - 40
+                        self.pos[200+c] = (xp, yp)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c+5), (xp, yp), self.size_cercle)
+                elif angle == 120:
+                    for c in range(6):
+                        xp = x + 23 * c + 23
+                        yp = y - 40 * c - 40
+                        self.pos[300+c] = (xp, yp)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c), (xp, yp), self.size_cercle)
+                elif angle == 180:
+                    for c in range(6):
+                        xp = x + 46 * c + 46
+                        self.pos[400+c] = (xp, y)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c+1), (xp, y), self.size_cercle)
+                elif angle == 240:
+                    for c in range(6):
+                        xp = x + 23 * c + 23
+                        yp = y + 40 * c + 40
+                        self.pos[500+c] = (xp, yp)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c+2), (xp, yp), self.size_cercle)
+                elif angle == 300:
+                    for c in range(6):
+                        xp = x - 23 * c - 23
+                        yp = y + 40 * c + 40
+                        self.pos[600+c] = (xp, yp)
+                        pygame.draw.circle(self.fenetre, self.color_choice(c+3), (xp, yp), self.size_cercle)
+                
+            self.player()
+
+            #centre TODO. marche po
+            pygame.draw.circle(self.fenetre, (255, 255, 255), (centre_x, centre_y), self.size_cercle)
 
 
-
-
-
-            pygame.draw.circle(self.fenetre, (255, 255, 255), (centre_x, centre_y), size_cercle)
-
-
-            # pygame.draw.circle(self.fenetre, purple, (centre_x, centre_y + 80), size_cercle)
-            # pygame.draw.circle(self.fenetre, yellow, (centre_x, centre_y + 160), size_cercle)
-            # pygame.draw.circle(self.fenetre, orange, (centre_x, centre_y + 240), size_cercle)
-            # pygame.draw.circle(self.fenetre, purple, (centre_x, centre_y - 80), size_cercle)
-            # pygame.draw.circle(self.fenetre, yellow, (centre_x-60, centre_y - 160), size_cercle)
-            # pygame.draw.circle(self.fenetre, orange, (centre_x - 120, centre_y - 220), size_cercle)
-
-            # pygame.draw.circle(self.fenetre, red, (centre_x + 80, centre_y), size_cercle)
-            # pygame.draw.circle(self.fenetre, green, (centre_x + 160, centre_y), size_cercle)
-            # pygame.draw.circle(self.fenetre, blue, (centre_x + 240, centre_y), size_cercle)
-            # pygame.draw.circle(self.fenetre, red, (centre_x - 80, centre_y), size_cercle)
-            # pygame.draw.circle(self.fenetre, green, (centre_x - 160, centre_y), size_cercle)
-            # pygame.draw.circle(self.fenetre, blue, (centre_x - 240, centre_y), size_cercle)
-
-
-            pygame.display.flip()  # Met à jour l'affichage
+            # pygame.display.flip()  # Met à jour l'affichage
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
         pygame.quit()
 
+    def move_player(self, new_pos):
+        self.pos_player = new_pos
+        pygame.draw.circle(self.fenetre, (255, 255, 255), (self.pos[self.pos_player][0], self.pos[self.pos_player][1]), 5)
+        pygame.display.flip()
+
+
+
+    def player(self):
+        if (self.pos_player == 43):
+            self.pos_player = 0
+        if(self.pos_player == -1):
+            self.pos_player = 42
+
+        # print(self.pos_player)
+
+        #print ("pos joueur: " + str(self.pos[self.pos_player][0]) + ", " + str(self.pos[self.pos_player][1]))
+
+        # print(str(self.pos_player) + " : (" + str(self.pos[self.pos_player%42][0]) + ", " + str(self.pos[self.pos_player%42][1]) + ")")
+
+
+        for event in pygame.event.get():
+            #met a jour l'affichage
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:  # Si un clic de souris est détecté
+                if event.button == 1:  # Vérifie si c'est le bouton gauche de la souris
+                    mouse_x, mouse_y = pygame.mouse.get_pos()  # Récupère la position de la souris
+                    for key, value in self.pos.items():
+                        x, y = value
+                        distance = math.sqrt((mouse_x - x) ** 2 + (mouse_y - y) ** 2)
+                        if distance < self.size_cercle:  # Si la souris est à l'intérieur du cercle
+                            print(f"Clic sur le cercle {key} à la position ({x}, {y}) avec la couleur {self.color_choice(key)}")
+                            self.move_player(key)
+                            break  # Sort de la boucle dès qu'un cercle est cliqué
+            elif event.type == pygame.KEYDOWN:  # Vérifie si une touche est enfoncée
+                if event.key == pygame.K_ESCAPE:  # Flèche de droite
+                    print("Fin du jeu")
+                    running = False
+                    pygame.quit()
+                # TODO : crash plus que ca quitte le jeu, a revoir !
+                pygame.display.flip()
+
+
+        # for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:
+        #             running = False
+        #         elif event.type == pygame.KEYDOWN:  # Vérifie si une touche est enfoncée
+        #             if event.key == pygame.K_UP:  # Flèche du haut
+        #                 print("La touche UP a été enfoncée.")
+        #                 self.pos_player -= 1
+        #             elif event.key == pygame.K_DOWN:  # Flèche du bas
+        #                 print("La touche DOWN a été enfoncée.")
+        #                 self.pos_player += 1
+        #             elif event.key == pygame.K_LEFT:  # Flèche de gauche
+        #                 if (self.pos_player == 0):
+        #                     self.pos_player = 100
+        #                 elif (self.pos_player == 8):
+        #                     self.pos_player = 200
+        #                 elif(self.pos_player == 15):
+        #                     self.pos_player = 300
+        #                     print("La touche LEFT a été enfoncée.")
+        #             elif event.key == pygame.K_RIGHT:  # Flèche de droite
+        #                 print("La touche RIGHT a été enfoncée.")
+        #             elif event.key == pygame.K_ESCAPE:  # Flèche de droite
+        #                 print("Fin du jeu")
+        #                 pygame.quit()
+        #                 # TODO : crash plus que ca quitte le jeu, a revoir !
+
+
+    
